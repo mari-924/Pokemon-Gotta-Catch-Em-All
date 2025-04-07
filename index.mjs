e5e66bf7a23a1037dd66c22e86b25642dacca3b9
mari-924 <marianaduran260@gmail.com>
2025-04-07 00:12:46 -0700

    added the pokemon and item search

--- index.mjs
+++ index.mjs
@@ -28,7 +28,7 @@ app.get('/', async (req, res) => {
     });
 });
 
-app.get('/RandomPokemon', async (req, res) => {
+app.get('/randomPokemon', async (req, res) => {
     const randomPokemonName = pokemon.random();
     let url = 'https://pokeapi.co/api/v2/pokemon/' + randomPokemonName.toLowerCase();
     let response = await fetch(url);
@@ -48,16 +48,29 @@ app.get('/RandomPokemon', async (req, res) => {
     });
 });
 
-app.get('/PokemonSearch', async (req, res) => {
-    const randomPokemonName = pokemon.random();
-    let url = 'https://pokeapi.co/api/v2/pokemon/' + randomPokemonName.toLowerCase();
+app.get('/pokemonSearch', async (req, res) => {
+    res.render('pokemonSearch');
+
+
+});
+
+app.get('/getPokemon', async (req, res) => {
+    let pokemonInput = req.query.pokemonInput; 
+
+    if (!pokemonInput) {
+        return res.send("Please provide a Pokémon name or ID.");
+    } 
+
+    pokemonInput = req.query.pokemonInput.toLowerCase(); 
+
+    let url = 'https://pokeapi.co/api/v2/pokemon/' + pokemonInput;
     let response = await fetch(url);
     let data = await response.json();
 
     console.log(data);
 
-    res.render('pokemonSearch', {
-        name: randomPokemonName,
+    res.render('displayPokemon', {
+        name: data.name,
         sprite: data.sprites.front_default,
         types: data.types.map(t => t.type.name),
         shiny: data.sprites.front_shiny,
@@ -65,6 +78,43 @@ app.get('/PokemonSearch', async (req, res) => {
         height: data.height,
         base_xp: data.base_experience,
         id: data.id
+    });
+});
+
+function getEffect(data) {
+    if (Array.isArray(data.effect_entries) && data.effect_entries.length > 0) {
+        return data.effect_entries[0].effect;
+    }
+    return "No effect description available.";
+}
+
+app.get('/itemSearch', async (req, res) => {
+    res.render('itemSearch');
+
+
+});
+
+app.get('/getItem', async (req, res) => {
+    let itemInput = req.query.itemInput; 
+
+    if (!itemInput) {
+        return res.send("Please provide a Item.");
+    } 
+
+    itemInput = req.query.itemInput.toLowerCase(); 
+
+    let url = 'https://pokeapi.co/api/v2/item/' + itemInput;
+    let response = await fetch(url);
+    let data = await response.json();
+
+    console.log(data);
+
+    res.render('displayItem', {
+        name: data.name,
+        sprite: data.sprites.default,
+        category: data.category.name,
+        effect: getEffect(data),
+        cost: data.cost
     });
 });
 
