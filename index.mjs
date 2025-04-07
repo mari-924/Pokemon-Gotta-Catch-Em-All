ba388c7e134bb0bbdb0140cd7faea5db072801c1
mari-924 <marianaduran260@gmail.com>
2025-04-06 22:02:27 -0700

    pokemon

---
+++ index.mjs
@@ -0,0 +1,27 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const pokemon = require('pokemon');
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res) => {
+    const randomPokemonName = pokemon.random();
+    let url = 'https://pokeapi.co/api/v2/pokemon/' + randomPokemonName.toLowerCase();
+    let response = await fetch(url);
+    let data = await response.json();
+
+    res.render('home', {
+        name: randomPokemonName,
+        sprite: data.sprites.front_default,
+        types: data.types.map(t => t.type.name)
+    });
+});
